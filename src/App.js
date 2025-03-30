8526f385b4bedfaf100f13abafbb65dcce15014b
rahul2668 <rahulramagiri27@gmail.com>
2025-03-31 02:41:32 +0530

    removed unwanted code

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+// import Footer from './components/Footer';
 import Home from './pages/Home';
 import GalleryPage from './components/GalleryPage';
 
